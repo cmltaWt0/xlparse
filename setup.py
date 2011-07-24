3eb713d579d930264e616868c1486c080e1ad441
Maksim Sokolskiy <maksim.sokolskiy@gmail.com>
2011-07-24 16:20:09 +0300

    Change .py to .pyw and change setup.py file

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from distutils.core import setup
+import sys
 import py2exe
 
-setup(console=['xlparse.py'])
+sys.argv.append('py2exe')
+sys.argv.append('-q')
+code_file = 'xlparse.pyw'
+setup(windows = [{'script': code_file}])
